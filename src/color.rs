016936c41323798fa29b9bb1b1b39fadd8377b3b
Clark Lindsay <clarklindsay96@gmail.com>
2019-06-14 20:28:47 -0400

    added a color + color addition

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,6 @@
+use core::ops::Add;
+use crate::float_cmp::ApproxEq;
+
 #[derive(Copy, Clone, Debug)]
 pub struct Color {
     r: f64,
@@ -16,6 +19,28 @@ pub fn new(r: f64, g: f64, b: f64) -> Color{
 impl Color {
 }
 
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b
+        }
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, other: &Self) -> bool {
+        self.r.approx_eq(other.r, (0.0, 2)) &&
+        self.g.approx_eq(other.g, (0.0, 2)) &&
+        self.b.approx_eq(other.b, (0.0, 2)) 
+    }
+}
+
+impl Eq for Color {}
+
 #[cfg(test)]
 mod color_tests {
     use crate::color;
@@ -30,5 +55,20 @@ mod color_tests {
         assert!(c1.r.approx_eq(-0.4, (0.0, 2)));
         assert!(c1.g.approx_eq(0.5, (0.0, 2)));
         assert!(c1.b.approx_eq(1.7, (0.0, 2)));
+    }
+
+    #[test]
+    fn add_colors() {
+        let red = color::new(1.0, 0.0, 0.0);
+        let green = color::new(0.0, 1.0, 0.0);
+        let blue = color::new(0.0, 0.0, 1.0);
+        let c1 = red + blue;
+        let c2 = green + blue;
+        let c3 = red + green;
+
+        assert_eq!(c1, color::new(1.0, 0.0, 1.0));
+        assert_eq!(c2, color::new(0.0, 1.0, 1.0));
+        assert_eq!(c3, color::new(1.0, 1.0, 0.0));
+
     }
 }
